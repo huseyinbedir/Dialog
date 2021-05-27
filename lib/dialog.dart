5b780dc57ea83968fa99b12362a6e7c715801a7d
Huseyin Bedir <hsynbdr@gmail.com>
2021-05-27 16:21:11 +0300

    initial commit

---
+++ lib/dialog.dart
@@ -0,0 +1,32 @@
+library dialog;
+
+import 'package:flutter/material.dart';
+
+class ShowDialog {
+  static Future showDialogGeneral({
+    @required BuildContext context,
+    @required bool status,
+  }) {
+    return showGeneralDialog(
+        barrierColor: Colors.black.withOpacity(0.5),
+        transitionBuilder: (context, a1, a2, widget) {
+          return Transform.scale(
+            scale: a1.value,
+            child: Opacity(
+              opacity: a1.value,
+              child: AlertDialog(
+                shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0)),
+                title: Text('Hello!!'),
+                content: Text('How are you?'),
+              ),
+            ),
+          );
+        },
+        transitionDuration: Duration(milliseconds: 200),
+        barrierDismissible: true,
+        barrierLabel: '',
+        context: context,
+        pageBuilder: (context, animation1, animation2) {});
+  }
+}
